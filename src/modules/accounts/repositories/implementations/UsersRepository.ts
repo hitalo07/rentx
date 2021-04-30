4cbfa5137def535318dc827dde5cff4e2802ee41
hitalo07 <hitalocunhadesousa@gmail.com>
2021-04-30 04:14:57 -0300

    feat: Update user avatar

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -11,18 +11,26 @@ class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User)
   }
 
-  async create({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+  async create ({ name, email, driver_license, password, avatar, id }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
       email,
       driver_license,
-      password
+      password,
+      avatar,
+      id
     })
     await this.repository.save(user)
   }
 
-  async findByEmail(email: string): Promise<User> {
+  async findByEmail (email: string): Promise<User> {
     const user = await this.repository.findOne({ email })
+    return user
+  }
+
+  async findById (id: string): Promise<User> {
+    const user = await this.repository.findOne(id)
+
     return user
   }
 
